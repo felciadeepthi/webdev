d9d0459d67737530f3719b1ed993187ad9b58d56
Deepthi <felciadeepthi@gmail.com>
2015-12-27 14:59:06 -0600

    angularjs week3 assignment

--- module3/week1/app/scripts/app.js
+++ module3/week1/app/scripts/app.js
@@ -1,178 +1,71 @@
 'use strict';
 
-angular.module('confusionApp', [])
+angular.module('confusionApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+        
+            // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html',
+                    },
+                    'content': {
+                        templateUrl : 'views/home.html',
+                        controller  : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html',
+                    }
+                }
 
+            })
+        
+            // route for the aboutus page
+            .state('app.aboutus', {
+                url:'aboutus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/aboutus.html',
+                        controller  : 'AboutController'                  
+                    }
+                }
+            })
+        
+            // route for the contactus page
+            .state('app.contactus', {
+                url:'contactus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/contactus.html',
+                        controller  : 'ContactController'                  
+                    }
+                }
+            })
 
+            // route for the menu page
+            .state('app.menu', {
+                url: 'menu',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/menu.html',
+                        controller  : 'MenuController'
+                    }
+                }
+            })
 
-.controller('DishDetailController', ['$scope', function ($scope) {
-  var dish = {
-    name: 'Uthapizza',
-    image: 'images/uthapizza.png',
-    category: 'mains',
-    label: 'Hot',
-    price: '4.99',
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-    comments: [
-      {
-        rating: 5,
-        comment: "Imagine all the eatables, living in conFusion!",
-        author: "John Lemon",
-        date: "2012-10-16T17:57:28.556094Z"
-                               },
-      {
-        rating: 4,
-        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-        author: "Paul McVites",
-        date: "2014-09-05T17:57:28.556094Z"
-                               },
-      {
-        rating: 3,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "2015-02-13T17:57:28.556094Z"
-                               },
-      {
-        rating: 4,
-        comment: "Ultimate, Reaching for the stars!",
-        author: "Ringo Starry",
-        date: "2013-12-02T17:57:28.556094Z"
-                               },
-      {
-        rating: 2,
-        comment: "It's your birthday, we're gonna party!",
-        author: "25 Cent",
-        date: "2011-12-02T17:57:28.556094Z"
-                               }
-
-                           ]
-  };
-
-  $scope.dish = dish;
-
-}])
-
-.controller('ContactController', ['$scope', function ($scope) {
-
-  $scope.feedback = {
-    mychannel: "",
-    firstName: "",
-    lastName: "",
-    agree: false,
-    email: ""
-  };
-
-  var channels = [{
-    value: "tel",
-    label: "Tel."
-  }, {
-    value: "Email",
-    label: "Email"
-  }];
-
-  $scope.channels = channels;
-  $scope.invalidChannelSelection = false;
-
-        }])
-
-.controller('FeedbackController', ['$scope', function ($scope) {
-
-  $scope.sendFeedback = function () {
-
-    console.log($scope.feedback);
-
-    if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
-      $scope.invalidChannelSelection = true;
-      console.log('incorrect');
-    } else {
-      $scope.invalidChannelSelection = false;
-      $scope.feedback = {
-        mychannel: "",
-        firstName: "",
-        lastName: "",
-        agree: false,
-        email: ""
-      };
-      $scope.feedback.mychannel = "";
-      $scope.feedbackForm.$setPristine();
-      console.log($scope.feedback);
-    }
-  };
-        }])
-
-.controller('DishDetailController', ['$scope', function ($scope) {
-
-  var dish = {
-    name: 'Uthapizza',
-    image: 'images/uthapizza.png',
-    category: 'mains',
-    label: 'Hot',
-    price: '4.99',
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-    comments: [
-      {
-        rating: 5,
-        comment: "Imagine all the eatables, living in conFusion!",
-        author: "John Lemon",
-        date: "2012-10-16T17:57:28.556094Z"
-                               },
-      {
-        rating: 4,
-        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-        author: "Paul McVites",
-        date: "2014-09-05T17:57:28.556094Z"
-                               },
-      {
-        rating: 3,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "2015-02-13T17:57:28.556094Z"
-                               },
-      {
-        rating: 4,
-        comment: "Ultimate, Reaching for the stars!",
-        author: "Ringo Starry",
-        date: "2013-12-02T17:57:28.556094Z"
-                               },
-      {
-        rating: 2,
-        comment: "It's your birthday, we're gonna party!",
-        author: "25 Cent",
-        date: "2011-12-02T17:57:28.556094Z"
-                               }
-
-                           ]
-  };
-
-  $scope.dish = dish;
-
-        }])
-
-.controller('DishCommentController', ['$scope', function ($scope) {
-
-  console.log($scope.newcomments);
-
-
-  $scope.newcomments = {
-    author: "",
-    comment: "",
-    rating: 5,
-    date: ""
-  };
-
-
-  $scope.submitComment = function () {
-    $scope.newcomments.date = Date.now();
-    $scope.dish.comments.push($scope.newcomments);
-    $scope.commentForm.$setPristine();
-    $scope.newcomments = {
-      author: "",
-      comment: "",
-      rating: 5,
-      date: ""
-    };
-  };
-
-
-}])
-
+            // route for the dishdetail page
+            .state('app.dishdetails', {
+                url: 'menu/:id',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/dishdetail.html',
+                        controller  : 'DishDetailController'
+                   }
+                }
+            });
+    
+        $urlRouterProvider.otherwise('/');
+    })
 ;
